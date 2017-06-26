9aaaddcf917d7ac853970cf1cd844ca0148f7891
Dionis Spinei <spinei.dionis@gmail.com>
2017-06-26 17:40:24 +0300

    fix `old_sound_rabbit_mq` configuration not properly detected when declared in multiple configuration files

--- DependencyInjection/RabbitMqSupervisorExtension.php
+++ DependencyInjection/RabbitMqSupervisorExtension.php
@@ -72,10 +72,12 @@ class RabbitMqSupervisorExtension extends Extension implements PrependExtensionI
                     $extensionConfig = $container->getExtensionConfig($name);
 
                     foreach (array('consumers', 'multiple_consumers') as $attribute) {
-                        if (isset($extensionConfig[0][$attribute])) {
-                            $attributeValue = $extensionConfig[0][$attribute];
-                        } else {
-                            $attributeValue = array();
+                        $attributeValue = array();
+
+                        foreach ($extensionConfig as $config) {
+                            if (isset($config[$attribute])) {
+                                $attributeValue = array_merge($attributeValue, $config[$attribute]);
+                            }
                         }
                         $container->setParameter('phobetor_rabbitmq_supervisor.' . $attribute, $attributeValue);
                     }
